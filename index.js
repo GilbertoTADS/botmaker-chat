0876604d833393d8186d59c3781a590932660673
Gilberto Carlos <gilbertotads@gmail.com>
2021-01-20 06:51:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,51 @@
+const api = require('./config/api.json')
+
+const { app, rp, myCache } = require('./server')
+
+const funcMessage = require('./actions/messages')( api, rp )
+
+const talk = require('./config/talk.json')
+
+const db = require('./database/connection')()
+
+const { getClientByCPF, getTenPurchasesThisClient } = require('./database/query')()
+
+const menu = require('./bot/menu.js')
+const { introduction, errorCPF, purchaseDetails } = require('./bot/mypurchases')()
+
+app.get('/',(req, res) => {
+
+    res.send('estamos em manutenção').end()
+
+})
+
+app.post('/',  async ( req, res ) => {
+
+    let { chatPlatform, contactId, message, customerId, fromName } = req.body
+
+    message = message ? message.replace(/ /g,'').toLowerCase() : undefined
+
+        if(myCache.get(contactId+'action')){
+
+            switch(myCache.get(contactId)){
+
+                case contactId+'mypurchases':
+                    introduction(message, getClientByCPF, myCache, funcMessage, db,contactId, talk)
+                break;
+
+                case contactId+'mypurchases-identified':
+                    errorCPF(message, contactId, myCache, getTenPurchasesThisClient, talk,db,funcMessage)
+                break;
+
+                case contactId+'mypurchases-successIdentified':
+                    purchaseDetails(message,contactId,talk,db,funcMessage,myCache,getTenPurchasesThisClient)
+                break; 
+
+            }
+        }
+
+    menu(contactId, message, myCache, talk, funcMessage)
+    
+    res.status(200).send('pagina encontrada').end()
+
+    })
---
+++ server.js
@@ -0,0 +1,27 @@
+const app = require('express')()
+
+const rp = require('request-promise')
+
+const cors = require('cors')
+
+const bodyParser = require('body-parser')
+
+
+
+const NodeCache =  require('node-cache')
+
+const myCache = new NodeCache( { stdTTL: 100, checkperiod: 120 } )
+
+
+
+app.use(bodyParser.json())
+
+app.use(cors())
+
+
+
+app.listen(3000,() => console.log('server started'))
+
+
+
+module.exports = { app,rp, myCache }
