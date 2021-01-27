21c74a778fdd77c59b7a7363e4231b479a8ccfbe
Gilberto Carlos <gilbertotads@gmail.com>
2021-01-27 09:30:04 -0300

    Update connection.js

--- src/database/connection.js
+++ src/database/connection.js
@@ -3,7 +3,7 @@ module.exports = async () => {
     const ibmdb = require('ibm_db')
     process.env.DB2CODEPAGE = 1208;
 
-    let connStr = "DATABASE=cisserp;HOSTNAME=192.168.1.135;PORT=50000;PROTOCOL=TCPIP;UID=dba;PWD=overhead";    
+    let connStr = "DATABASE=database;HOSTNAME=host;PORT=50000;PROTOCOL=TCPIP;UID=user;PWD=pass";    
 
     const connect = ibmdb.open(connStr)
     return  connect
